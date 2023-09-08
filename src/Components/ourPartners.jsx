703813f4ddf1b374566cf88e42774e935ebdc403
Ajmal Wali <awsyal2002@gmail.com>
2023-09-08 18:20:50 +0430

    our partners page created

--- src/Components/ourPartners.jsx
+++ src/Components/ourPartners.jsx
@@ -1,3 +1,15 @@
 export function OurPartners() {
-  return <>Hello</>;
+  return (
+    <div className="text-primary card shadow-xl mt-2 m-3 p-9 bg-accent overflow-x-hidden h-fit lg:mt-9 lg:m-11">
+      <h1 className="text-3xl font-medium text-center">Our Partners</h1>
+      <card className="mt-12 flex items-center justify-center">
+        <a href="https://omid.edu.af/" target="_blank" rel="noreferrer">
+          <img
+            className="rounded-full w-36 h-36"
+            src="/imgs/partners/omid-logo.png"
+          />
+        </a>
+      </card>
+    </div>
+  );
 }
